503281de7410300ffc9f1ee239e0e71be053ebe8
maviemavi <mavistudy428@gmail.com>
2024-11-22 22:46:43 -0300

    Update Registro.cpp

--- EP2/Registro/Registro.cpp
+++ EP2/Registro/Registro.cpp
@@ -1,10 +1,12 @@
 #include "Registro.h"
 
 Registro::Registro(Data* d, bool entrada, bool manual) {
+if (d==nullptr){ throw std::invalid_argument("data não pode ser um dia nulo");
+}
     this->data = d;
     this->entrada = entrada;
     this->manual = manual;
-}
+    }
 
 Registro::~Registro() {
     delete data;
